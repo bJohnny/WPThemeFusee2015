8aae2ceea595b1353c014d2f786e95facae8f9dd
John <der.jbrown@gmail.com>
2015-08-27 16:14:49 +0200

    Dings

--- templates/page-full.php
+++ templates/page-full.php
@@ -6,6 +6,29 @@ get_header(); ?>
 <div class="row">
 	<div class="small-12 large-12 columns" role="main">
 
+
+          <ul id="featured1" data-orbit data-options="timer_speed:5000;">
+            <li>
+              <img src="http://foundation.zurb.com/docs/assets/img/examples/satelite-orbit.jpg" alt=""/>
+              <div class="orbit-caption">
+                Caption One. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+              </div>
+            </li>
+            <li>
+              <img src="http://foundation.zurb.com/docs/assets/img/examples/andromeda-orbit.jpg" alt=""/>
+              <div class="orbit-caption">
+                Caption Two. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+              </div>
+            </li>
+            <li>
+              <img src="http://foundation.zurb.com/docs/assets/img/examples/launch-orbit.jpg" alt=""/>
+              <div class="orbit-caption">
+                Caption Three. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.
+              </div>
+            </li>
+          </ul>
+
+
 	<?php /* Start loop */ ?>
 	<?php while ( have_posts() ) : the_post(); ?>
 		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
